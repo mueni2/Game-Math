a95563f28d522b0895ff83b49d2d6e0ee27e63b7
mueni2 <anmomoan@gmail.com>
2023-01-03 16:16:28 +0000

    Added code to calculate two randon numbers

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 //console.log("connected!")
-ddocument.addEventListener("DOMContentLoaded", function() {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
@@ -12,36 +15,46 @@ ddocument.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
 });
 
-function runGame(params) {
-    
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
 }
 
-function checkAnswer(params) {
-    
+
+function checkAnswer() {
+
 }
 
-function calculateCorrectAnswer(params) {
-    
+function calculateCorrectAnswer() {
+
 }
 
-function incrementScore(params) {
-    
+function incrementScore() {
+
 }
 
-function incrementWrongAnswer(params) {
-    
+function incrementWrongAnswer() {
+
 }
 
-function displayAdditionQuestion(params) {
-    
+function displayAdditionQuestion(operand1, operand2) {
+
 }
 
-function displayMultiplyQuestion(params) {
-    
+function displaySubtractQuestion() {
+
 }
 
-function displaySubstractQuestion(params) {
+function displayMultiplyQuestion() {
     
 }
