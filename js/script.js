b77e8cdc56e6d8c60f7e6ff2a04cf214b6af2755
mueni2 <anmomoan@gmail.com>
2023-01-03 15:12:34 +0000

    set up structure of game functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 //console.log("connected!")
+
+function runGame(params) {
+    
+}
+
+function checkAnswer(params) {
+    
+}
+
+function calculateCorrectAnswer(params) {
+    
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAnswer(params) {
+    
+}
+
+function displayAdditionQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
+
+function displaySubstractQuestion(params) {
+    
+}
